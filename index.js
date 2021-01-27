51cbd2c566ac1082a5c0001048d3a0666cf96118
iodar <granzowdario@gmail.com>
2021-01-27 11:54:06 +0100

    feature: improved logging for better showcase of env vars in node app

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // logging environment values from process.env (hosts environment variables)
-console.log(
-  `>>> FROM NODE (index.js) process.env.FOOBAR: ${process.env.FOOBAR}`
-);
+console.log("\n--- NODE.JS inside docker container ---\n");
+console.log(`>>> FROM NODE (index.js) process.env.FOOBAR: ${process.env.FOOBAR}`);
 console.log(`>>> FROM NODE (index.js) process.env.OTHER: ${process.env.OTHER}`);
+console.log("\n");
